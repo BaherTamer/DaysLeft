75a75259c83c568f45f823eddfc745eb37bbc159
BaherTamerHashem <baher.appledev@gmail.com>
2023-01-17 19:46:34 +0200

    Creating starter project

---
+++ DaysLeft/DaysLeft/DaysLeftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysLeftApp.swift
+//  DaysLeft
+//
+//  Created by Baher Tamer on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DaysLeftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
